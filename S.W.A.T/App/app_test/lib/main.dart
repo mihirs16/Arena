0b0b31f9701e5c6c96f02c1dea2fb81812c02c5c
rushilrai <58342835+rushilrai@users.noreply.github.com>
2020-03-09 03:31:38 +0530

    Update main.dart

--- S.W.A.T/App/app_test/lib/main.dart
+++ S.W.A.T/App/app_test/lib/main.dart
@@ -1,1 +1,99 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Arena',
+      home: Scaffold(
+        body: Center(
+  child: Column(
+  children: [
+     SizedBox(
+      height: 150.0
+    ),
+    Container(
+      decoration: BoxDecoration(
+       color: Colors.greenAccent,
+       borderRadius: BorderRadius.all(Radius.circular(15.0)), 
+       boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0.0, 5.0),
+              spreadRadius: 2.0,
+              blurRadius: 15.0,
+            )
+          ],
+      ),
+      
+      width: 330.0,
+      height: 200.0,
+     
+    ),
+    SizedBox(
+      height: 50.0
+    ),
+    Container(
+      decoration: BoxDecoration(
+       color: Colors.redAccent[400], 
+       borderRadius: BorderRadius.all(Radius.circular(15.0)),
+       boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0.0, 5.0),
+              spreadRadius: 2.0,
+              blurRadius: 15.0,
+            )
+          ], 
+      ),
+      width: 330.0,
+      height: 60.0,
+     
+    ),
+    SizedBox(
+      height: 50.0
+    ),
+    Container(
+      decoration: BoxDecoration(
+       color: Colors.greenAccent, 
+       borderRadius: BorderRadius.all(Radius.circular(15.0)),
+       boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0.0, 5.0),
+              spreadRadius: 2.0,
+              blurRadius: 15.0,
+            )
+          ], 
+      ),
+      width: 330.0,
+      height: 200.0,
+    ),
+     SizedBox(
+      height: 50.0
+    ),
+    Container(
+      decoration: BoxDecoration(
+       color: Colors.greenAccent, 
+       borderRadius: BorderRadius.all(Radius.circular(15.0)), 
+       boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0.0, 5.0),
+              spreadRadius: 2.0,
+              blurRadius: 15.0,
+            )
+          ],
+      ),
+      width: 330.0,
+      height: 60.0,
+    ),
+  ],
+)
+      ),
+      ),
+    );
+  }
+}
