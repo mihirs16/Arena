0895b45f5bf3222ddb39b679d4239d54918a6631
Mihir Singh <mihirs16@gmail.com>
2020-03-20 11:27:07 +0530

    API Data Object setup

--- S.W.A.T/App/app_test/lib/api_call_season.dart
+++ S.W.A.T/App/app_test/lib/api_call_season.dart
@@ -1,14 +1,54 @@
 import 'dart:convert';
 import 'dart:async';
+import 'dart:ffi';
 import 'package:http/http.dart' as http;
 
-class Team {
-  var id;
-  var name;
-  var imageUrl;
+class TeamStats {
+  int teamID;
+  int posTable;
+  String last5rec;
+  Float scoredRatio;
+  Float concedRatio;
+  Match lastPlayedMatch;
+  Match upcomingMatch;
+
+  TeamStats(var decodedJsonResponse) {
+    //parse and save from json
+  }
 }
 
+class Team {
+  var teamID;
+  var name;
+  var imageUrl;
+
+  Team(var decodedJsonResponse) {
+    //parse and save from json
+  }
+}
+
+class Match{
+  int id;
+  Team homeTeam;
+  Team awayTeam;
+  int status;
+  int homeScore;
+  int awayScore;
+  DateTime dateTime;
+
+  Match(var decodedJsonResponse) {
+    //parse and save from json
+  }
+}
+
+
+//---GLOBAL-----
+var teamsOfTheSeason = new List<Team>(20);
+var teamStatsOfTeams = new List<TeamStats>(20);
+
+
 Future<List<Team>> getTeamDataAtStart() async {
+  print('Fetching Season Contenders..');
   http.Response response = await http.get(
     Uri.encodeFull("https://api.footystats.org/league-teams?key=test85g57&league_id=2012"),
     headers: {
@@ -32,4 +72,24 @@ Future<List<Team>> getTeamDataAtStart() async {
   //for (int i=0;i<teams.length;i++)
   //  print(teams[i].name);             //UNCOMMENT 48 and 49 for debug
   return teams;
+}
+
+void getMatchData() async {
+  print('Fetching Match..');
+  http.Response response = await http.get(
+    Uri.encodeFull("https://api.footystats.org/league-tables?key=test85g57&league_id=2012"),
+    headers: {
+      "Accept": "application/json",
+    }
+  );
+
+  var _league_matches = json.decode(response.body);
+  print(_league_matches['data']['league_table'][0]);
+  // var league_matches = _league_matches['data'];
+  
+  // var match = new Match();
+  // match.id = league_matches[0]['id'];
+
+  // print(league_matches[0]['homeID']);
+  // print(league_matches[0]['awayID']);
 }
